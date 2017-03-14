dc24b4519e8580bfae98393729fbfcfcad63c4e4
shannon594 <shannon.hoffman@washburn.edu>
2017-03-14 05:34:06 -0500

    Save before Android Studio Update.

--- app/src/main/java/com/example/zzmech/wumap/WarningFrag.java
+++ app/src/main/java/com/example/zzmech/wumap/WarningFrag.java
@@ -1,8 +1,10 @@
 package com.example.zzmech.wumap;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,11 +19,11 @@ import android.widget.Toast;
 
 public class WarningFrag extends Fragment
 {
-    private TextView editText;
     private TextView textView;
     private TextView textView1;
-    private Button saveBut;
-    private WebView wv;
+    private Button intentBut;
+
+    final String action = Settings.ACTION_LOCATION_SOURCE_SETTINGS;
 
     public WarningFrag()
     {
@@ -38,6 +40,19 @@ public class WarningFrag extends Fragment
                              Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_warning_frag, parent, false);
+
+        intentBut = (Button)v.findViewById(R.id.intentButton);
+
+        intentBut.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                getActivity().startActivity(new Intent(action));
+            }
+        });
+
+
 
         return v;
     }
